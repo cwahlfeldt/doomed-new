08ead69dd1ef8133c7834bb47b8e593d7fbe1c28
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-03-25 11:47:55 -0500

    removed client side graphql calls

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,34 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import '../assets/tachyons/css/tachyons.min.css';
 
-import Nav from '../components/Nav'
-import './index.css'
-
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-const client = new ApolloClient({ uri: 'http://dev-doomed.pantheonsite.io/graphql' });
+import Nav from '../components/Nav';
+import './index.css';
 
 const TemplateWrapper = ({ children }) => (
-  <ApolloProvider client={client}>
-    <section className="template-wrap">
-      <Helmet>
-        <title>Doomed</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-      </Helmet>
-      <Nav />
-      <main>
-        { children() }
-      </main>
-    </section>
-  </ApolloProvider>
-)
+  <section className="template-wrap">
+    <Helmet>
+      <title>Doomed</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    </Helmet>
+    <Nav />
+    <main>
+      { children() }
+    </main>
+  </section>
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,13 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
-
-const pageQuery = gql`
-  query pageQuery {
-     posts {
-      edges {
-        node {
-          id
-          title
-          uri
-          content
-        }
-      }
-    }
-  }
-`;
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = ({ data: {wordpressPost} }) => (
-  <div>
-  <Query query={pageQuery}>
-    {({ loading, error, data }) => {
-      if (loading) return <div>Loading...</div>;
-      if (error) return <div>Error :(</div>;
-
-      return (
-        <div>
-          <p>{ data.posts.edges[0].node.title }</p>
-        </div>
-      )
-    }}
-  </Query>
+  <section className={``}>
     <p>{ wordpressPost.title }</p>
-  </div>
-)
+  </section>
+);
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
   query mypagequery {
